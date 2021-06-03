9c1a291302763a41c75c465e08787fceb9576ee2
Мельниченко Давид <125-19-1-12-melnichenko@knute.edu.ua>
2021-06-03 10:14:23 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread fibonacci = new Thread(Fibonacci);
+            Thread prime = new Thread(Prime);
+
+            fibonacci.Start();
+            prime.Start();
+
+            fibonacci.Join();
+            foreach (int num in fibonacci_nums)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine($"{fibonacci_nums.Count} чисел Фiбоначчі");
+
+            prime.Join();
+            foreach (int num in primes)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine($"{primes.Count} простих чисел");
+
+        }
+
+        static List<int> fibonacci_nums = new List<int>();
+        static List<int> primes = new List<int>();
+
+        static void Fibonacci()
+        {
+
+        }
+        static void Prime()
+        {
+
+        }
+    }
+}
